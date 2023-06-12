2479511a607bdfa5ea517868132df6d02d304ee9
Sayan Dey <55090576+making-newway@users.noreply.github.com>
2023-06-12 12:53:46 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'June 12, 2023',
+  birthdate: 'June 13, 2023',
   name: 'Dear Kiki'
 };
 
